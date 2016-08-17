7eb39172024e99fc47928bacf13670a9a2f6f74e
Manoel Braz Maciel <manoelbraz.maciel@gmail.com>
2016-08-16 23:27:24 -0300

    Capítulo 3 - While

---
+++ capitulo3/while.java
@@ -0,0 +1,18 @@
+class ComandoWhile  {
+
+	public static void main (String[] args) {
+
+		System.out.println("O Comando While ... !");
+		
+		int idade = 15;
+			
+		while (idade < 18) {
+
+			System.out.println(idade + 1);
+			idade = idade + 1;
+
+		}
+
+	}
+
+}
